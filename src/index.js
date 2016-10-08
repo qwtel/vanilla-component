e6cb91ca2f1a40940c924af4aba2b244937f0d38
Florian Klampfer <f.klampfer@gmail.com>
2016-10-08 16:46:15 +0200

    Version 4

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@
  * Copyright (c) 2016 Florian Klampfer
  * Licensed under MIT
  */
-import componentCore from './componentCore';
-import defineJQueryComponent from './defineJQueryComponent';
-import htmlElement from './htmlElement';
+import componentCore from './component-core';
+import defineJQueryComponent from './define-jquery-component';
+import htmlElement from './html-element';
 
 export {
   componentCore,
